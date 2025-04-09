7e9ee287e5c50d10fbd56a70779a6c02ef449294
Metin Türk <metinturk553@gmail.com>
2025-04-09 21:57:18 +0300

    Bugun bir bağlı doğrusal listeler çalıştım.

---
+++ listeler/birBagliDogrusalListe/bbdol.cpp
@@ -0,0 +1,214 @@
+#include <iostream>
+#include <locale.h>
+#include <conio.h>
+#include "bbdol.h"
+
+using namespace std;
+
+
+
+void dumplist(node* list)
+{
+	int i = 1;
+	while(list != NULL)
+	{
+		cout << "Listenin " << i++ << ". Nodunun Adresi= " << list 
+							<<" Datasi=" << list -> data
+							<<" Linki = " << list -> link << endl;
+		list = list -> link;
+	}
+}
+//--------------------------------------------
+node* newnode()
+{
+	node* newnode = new node;
+	newnode -> link = NULL;
+	return(newnode);
+}
+//--------------------------------------------
+node* last(node* list)
+{
+	if(list != NULL)
+	{
+		while(list -> link != NULL)
+		{
+			list = list -> link;
+		}
+	}
+	node* last = list;
+	return(last);
+}
+//-------------------------------------------
+void addhead(node* node_ , node*& list)
+{
+	node_ -> link = list;
+	list = node_;
+}
+//------------------------------------------
+
+void concatenate(node*& l1 , node* l2)
+{
+	if(l1 == NULL)
+	{
+		l1 = l2;
+	}
+	else
+	{
+		last(l1) -> link = l2;
+	}
+}
+//------------------------------------------
+
+node* cons(int data_)
+{
+	node* l1 = newnode();
+	l1 -> data = data_;
+	return(l1);
+}
+//-----------------------------------------
+
+node* copy(node* list)
+{
+	node* l1 = newnode();
+	if(list != NULL)
+	{
+		do
+		{
+			concatenate(l1 , cons(list -> data));
+			list = list -> link;
+		}while(list != NULL);
+	}
+	return(l1);
+}
+//-----------------------------------------
+
+node* locate(int data_ , node* list)
+{
+	node* locate = NULL;
+	while(list != NULL)
+	{
+		if(list -> data != data_)
+		{
+			list = list -> link;
+		}
+		else
+		{
+			locate = list;
+			break;
+		}
+	}
+	return(locate);
+}
+//----------------------------------------
+
+bool member(node* node_ , node* list)
+{
+	while(list != NULL && list != node_)
+	{
+		list = list -> link;
+	}
+	bool member = (list == node_);
+	return member;
+}
+
+node* cuthead(node*& list)
+{
+	node* cuthead = list;
+	if(list != NULL)
+	{
+		list = list -> link;
+		cuthead -> link = NULL;
+	}
+	return(cuthead);
+}
+//---------------------------------------
+
+void cutlast(node*& list)
+{
+	node* sonnode = last(list);
+	node* baslangic = list;
+	while(list != NULL)
+	{
+		if(list -> link == sonnode)
+		{
+			list -> link = NULL;
+		}
+		else
+		{
+			list = list -> link;
+		}
+	}
+	list = baslangic;
+}
+//--------------------------------------
+
+void free(node*& list)
+{
+	delete list;
+}
+//-------------------------------------
+
+bool advance(node*& list)
+{
+	bool advance = false;
+	if(list != NULL && list -> link != NULL)
+	{
+		list = list -> link;
+		advance = true;
+	}
+	return(advance);
+}
+//---------------------------------------------------
+
+bool deletenode(node* node_ , node*& list)
+{
+	bool deletenode = false;
+	if(list == NULL)
+	{
+		return(deletenode);
+	}
+	if(list == node_)
+	{
+		node* silinecek = cuthead(list);
+		free(silinecek);
+		deletenode = true;
+		return(deletenode);
+	}
+	else
+	{
+		node* point = list;
+		do
+		{
+			if(point -> link == node_)
+			{
+				node* silinecek = cuthead(point->link);
+				free(silinecek);						//free fonks. bizden gerçek bir deðiþken istiyor.
+				deletenode = true;
+				return(deletenode);
+			}
+		}while(advance(point));
+		
+	}
+	return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ listeler/birBagliDogrusalListe/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include "bbdol.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL ,"Turkish");
+	
+	/*
+	node* liste = newnode();
+	
+	cout << liste << endl;
+	cout << liste -> data << endl;
+	cout << liste -> link << endl;
+	
+	liste -> data = 5;
+	dumplist(liste);
+	*/
+	/*
+	node* list;
+	
+	node* l1 = cons(25);
+	
+	concatenate(list , l1);
+	dumplist(list);
+	
+	node* l2 = cons(85);
+	concatenate(l1 , l2);
+	dumplist(l1);
+	*/
+	
+	
+	node* list = NULL;
+	
+	node* l1 = cons(85);
+	addhead(cons(25),  l1);
+	addhead(cons(47) , l1);
+	node* l2 = NULL;
+	l2 = copy(l1);	
+	dumplist(l1);
+	cout << "*-----------------------------*" << endl;
+	dumplist(l2);
+	cout << bool(member(l1, l2))<< endl;
+	if(locate(26 , l1) != NULL)
+	{
+		cout << "Aradýðýnýz node adresi: " << locate(26 , l1) << endl;
+	}
+	else
+	{
+		cout << "Aradýðýnýz node yok." << endl;
+	}
+	
+	cutlast(l2);
+	dumplist(l2);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	node* list = NULL;
+	
+	node* l1 = newnode();
+	l1 -> data = 10;
+	addhead(l1,list);
+	
+	node* l2 = newnode();
+	l2 -> data = 20;
+	addhead(l2,list);
+	
+	cout << "Son node adresi: " << last(list) << endl;
+	dumplist(list);
+	*/
+	system("pause");
+	return 0;
+}
